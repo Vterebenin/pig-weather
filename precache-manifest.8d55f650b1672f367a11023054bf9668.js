b7926cbdde468b6da318c22273c338e04ad32ea4
Vterebenin <niceserver11@mail.ru>
2019-09-29 23:59:26 +0300

    init

---
+++ precache-manifest.8d55f650b1672f367a11023054bf9668.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d924dd3d1cd715691438",
+    "url": "/js/about.a687842f.js"
+  },
+  {
+    "revision": "9f49d0671ab011c50f04",
+    "url": "/css/app.6a798533.css"
+  },
+  {
+    "revision": "9f49d0671ab011c50f04",
+    "url": "/js/app.62a76064.js"
+  },
+  {
+    "revision": "571ab1f170d058f73a2b",
+    "url": "/css/chunk-vendors.839175dd.css"
+  },
+  {
+    "revision": "571ab1f170d058f73a2b",
+    "url": "/js/chunk-vendors.b2dbd1c4.js"
+  },
+  {
+    "revision": "55397b643a280f0c951816adeaab3fa5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
